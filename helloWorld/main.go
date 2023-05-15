c0c7557e01498ba864e401d2837ec64b7cdb513a
matias-gallo <matias.a.gallo.96@gmail.com>
2023-05-15 16:43:27 -0300

    Declarations and looping

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -3,5 +3,27 @@ package main // Executable package !
 import "fmt"
 
 func main() {
-	fmt.Println("Hi there!")
+	// Static Typing ! Can be infered
+	// var card string = "Ace of Spades" - Equivalent complete declaration
+	firstCard := "Ace of Spades"
+	fmt.Println(firstCard)
+
+	secondCard := newCard()
+	fmt.Println(secondCard)
+
+	// Array fixed length
+	// Slice - can grow and shrink (List!)
+	cards := []string{"Ace of Diamonds", newCard()}
+	// Returns new slice
+	cards = append(cards, "Six of Spades")
+
+	// Destructuring - Clearing i and card every loop
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+// Explicit data type returns
+func newCard() string {
+	return "Five of Diamonds"
 }
